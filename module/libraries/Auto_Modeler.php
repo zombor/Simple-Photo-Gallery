0be6b0ccb6dc9849b18a44741a3733c9d8d0a062
Jeremy Bush <contractfrombelow@gmail.com>
2010-02-11 22:44:07 -0600

    Adding new auto modeler for 2.4 version.

--- module/libraries/Auto_Modeler.php
+++ module/libraries/Auto_Modeler.php
@@ -4,7 +4,7 @@
 *
 * @package        Auto_Modeler
 * @author         Jeremy Bush
-* @copyright      (c) 2008 Jeremy Bush
+* @copyright      (c) 2010 Jeremy Bush
 * @license        http://www.opensource.org/licenses/isc-license.txt
 */
 class Auto_Modeler_Core extends Model implements ArrayAccess
@@ -25,6 +25,8 @@ class Auto_Modeler_Core extends Model implements ArrayAccess
 
 	protected $validated = FALSE;
 
+	protected $lang = 'form_errors';
+
 	public function __construct($id = NULL)
 	{
 		parent::__construct();
@@ -32,7 +34,7 @@ class Auto_Modeler_Core extends Model implements ArrayAccess
 		if ($id != NULL)
 		{
 			// try and get a row with this ID
-			$data = $this->db->getwhere($this->table_name, array('id' => $id))->result(FALSE);
+			$data = db::builder()->from($this->table_name)->where('id', '=', $id)->execute($this->db)->as_array();
 
 			// try and assign the data
 			if (count($data) == 1 AND $data = $data->current())
@@ -62,6 +64,17 @@ class Auto_Modeler_Core extends Model implements ArrayAccess
 			$this->data[$key] = $value;
 	}
 
+	public function __sleep()
+	{
+		// Store only information about the object without db property
+		return array_diff(array_keys(get_object_vars($this)), array('db'));
+	}
+
+	public function __wakeup()
+	{
+		$this->db = Database::instance($this->db);
+	}
+
 	public function as_array()
 	{
 		return $this->data;
@@ -86,7 +99,7 @@ class Auto_Modeler_Core extends Model implements ArrayAccess
 				$key = $this->aliases[$key];
 
 			if (array_key_exists($key, $this->data))
-				$this->data[$key] = $value;
+				$this->$key = $value;
 		}
 	}
 
@@ -120,7 +133,7 @@ class Auto_Modeler_Core extends Model implements ArrayAccess
 		else
 		{
 			$this->validation = $data;
-			$errors = View::factory('form_errors')->set(array('errors' => $data->errors('form_errors')));
+			$errors = View::factory('form_errors')->set(array('errors' => $data->errors($this->lang)));
 			return $errors->render();
 		}
 	}
@@ -133,10 +146,10 @@ class Auto_Modeler_Core extends Model implements ArrayAccess
 		if ($status === TRUE)
 		{
 			if ($this->data['id']) // Do an update
-				return count($this->db->update($this->table_name, $this->data, array('id' => $this->data['id'])));
+				return count(db::update($this->table_name, array_diff_assoc($this->data, array('id' => $this->data['id'])), array('id' => $this->data['id'])));
 			else // Do an insert
 			{
-				$id = $this->db->insert($this->table_name, $this->data)->insert_id();
+				$id = db::insert($this->table_name, $this->data)->insert_id();
 				return ($this->data['id'] = $id);
 			}
 		}
@@ -149,7 +162,7 @@ class Auto_Modeler_Core extends Model implements ArrayAccess
 	{
 		if ($this->data['id'])
 		{
-			return $this->db->delete($this->table_name, array('id' => $this->data['id']));
+			return db::delete($this->table_name, array('id' => $this->data['id']));
 		}
 	}
 
@@ -158,7 +171,7 @@ class Auto_Modeler_Core extends Model implements ArrayAccess
 	// $direction - the direction to sort
 	public function fetch_all($order_by = 'id', $direction = 'ASC')
 	{
-		return $this->db->orderby($order_by, $direction)->get($this->table_name)->result(TRUE, inflector::singular(ucwords($this->table_name)).'_Model');
+		return db::builder()->from($this->table_name)->order_by($order_by, $direction)->execute($this->db)->as_object(inflector::singular(ucwords($this->table_name)).'_Model');
 	}
 
 	// Does a basic search on the table.
@@ -166,10 +179,10 @@ class Auto_Modeler_Core extends Model implements ArrayAccess
 	// $order_by - the ORDER BY value to sort by
 	// $direction - the direction to sort
 	// $type - pass 'or' here to do a orwhere
-	public function fetch_where($where = array(), $order_by = 'id', $direction = 'ASC', $type = '')
+	public function fetch_where($where = array(), $order_by = 'id', $direction = 'ASC', $type = 'and')
 	{
-		$function = $type.'where';
-		return $this->db->$function($where)->orderby($order_by, $direction)->get($this->table_name)->result(TRUE, inflector::singular(ucwords($this->table_name)).'_Model');
+		$function = $type.'_where';
+		return db::builder()->from($this->table_name)->$function($where)->order_by($order_by, $direction)->execute($this->db)->as_object(inflector::singular(ucwords($this->table_name)).'_Model');
 	}
 
 	// Returns an associative array to use in dropdowns and other widgets
@@ -181,7 +194,7 @@ class Auto_Modeler_Core extends Model implements ArrayAccess
 	{
 		$rows = array();
 
-		$query = empty($where) ? $this->fetch_all($order_by) : $this->db->where($where)->orderby($order_by)->get($this->table_name)->result(TRUE, inflector::singular(ucwords($this->table_name)).'_Model');
+		$query = empty($where) ? $this->fetch_all($order_by) : $this->fetch_where($where, $order_by);
 
 		$array_display = is_array($display);
 
--- module/libraries/Auto_Modeler_ORM.php
+++ module/libraries/Auto_Modeler_ORM.php
@@ -17,23 +17,24 @@ class Auto_Modeler_ORM_Core extends Auto_Modeler
 	{
 		$table = isset($this->aliases[$key]) ? $this->aliases[$key] : $key;
 
-		// See if we are requesting a forgien key
+		// See if we are requesting a foreign key
 		if (isset($this->data[$key.'_id']))
-			// Get the row from the forgien table
-			return $this->db->from($table.'s')->where('id', $this->data[$key.'_id'])->get()->result(TRUE, inflector::singular(ucwords($table)).'_Model')->current();
+			// Get the row from the foreign table
+			return $this->db->from(inflector::plural($table))->where('id', $this->data[$key.'_id'])->get()->result(TRUE, inflector::singular(ucwords($table)).'_Model')->current();
 		else if (isset($this->data[$key]))
 			return $this->data[$key];
 	}
 
 	public function __set($key, $value)
 	{
+		$original_key = $key;
 		if (isset($this->aliases[$key]))
 			$key = $this->aliases[$key];
 
-		if (in_array($key, $this->has_many))
+		if (in_array($original_key, $this->has_many))
 		{
 			$this_key = inflector::singular($this->table_name).'_id';
-			$f_key = inflector::singular($key).'_id';
+			$f_key = inflector::singular($original_key).'_id';
 
 			// See if this is already in the join table
 			if ( ! count($this->db->getwhere($this->table_name.'_'.$key, array($f_key => $value, $this_key => $this->data['id']))))
@@ -42,7 +43,7 @@ class Auto_Modeler_ORM_Core extends Auto_Modeler
 				$this->db->insert($this->table_name.'_'.$key, array($f_key => $value, $this_key => $this->data['id']));
 			}
 		}
-		else if (in_array($key, $this->belongs_to))
+		else if (in_array($original_key, $this->belongs_to))
 		{
 			$this_key = inflector::singular($this->table_name).'_id';
 			$f_key = inflector::singular($key).'_id';
@@ -72,57 +73,61 @@ class Auto_Modeler_ORM_Core extends Auto_Modeler
 		}
 	}
 
-	public function find_related($key, $where = array(), $orderby = array())
+	public function find_related($key, $where = array(), $order_by = 'id', $order = 'ASC')
 	{
+		$orig_key = $key;
 		if (isset($this->aliases[$key]))
 			$key = $this->aliases[$key];
 		$table_name = isset($this->aliases[$this->table_name]) ? $this->aliases[$this->table_name] : $this->table_name;
 		$model = inflector::singular($key).'_Model';
+
 		$temp = new $model();
 		if ($temp->has_attribute(inflector::singular($table_name).'_id')) // Look for a one to many relationship
-			return $this->db->from($key)->orderby($orderby)->where($where + array(inflector::singular($table_name).'_id' => $this->data['id']))->get()->result(TRUE, inflector::singular(ucwords($key)).'_Model');
+			return $this->db->from($key)->orderby($order_by, $order)->where($where + array(inflector::singular($table_name).'_id' => $this->data['id']))->get()->result(TRUE, inflector::singular(ucwords($key)).'_Model');
 		else // Get a many to many relationship
 		{
 			$join_table = $table_name.'_'.$key;
 			$this_key = inflector::singular($table_name).'_id';
-			$f_key = inflector::singular($key).'_id';
+			$f_key = inflector::singular($orig_key).'_id';
 
 			return $this->db->select($key.'.*')->from($key)->where($where + array($join_table.'.'.$this_key => $this->data['id']))->join($join_table, $join_table.'.'.$f_key, $key.'.id')->get()->result(TRUE, inflector::singular(ucwords($key)).'_Model');
 		}
 	}
 
-	public function find_parent($key = NULL, $where = array(), $orderby = array())
+	public function find_parent($key = NULL, $where = array(), $order_by = 'id', $order = 'ASC')
 	{
+		$orig_key = $key;
 		if (isset($this->aliases[$key]))
 			$key = $this->aliases[$key];
 		$table_name = isset($this->aliases[$this->table_name]) ? $this->aliases[$this->table_name] : $this->table_name;
 
 		if ($this->has_attribute($key.'_id')) // Look for a one to many relationship
 		{
-			return $this->db->from(inflector::plural($table_name))->orderby($orderby)->where($where + array('id' => $this->data[$key.'_id']))->get()->result(TRUE, ucwords($table_name).'_Model');
+			return $this->db->from(inflector::plural($table_name))->where($where + array('id' => $this->data[$key.'_id']))->get()->result(TRUE, ucwords($table_name).'_Model');
 		}
 		else
 		{
 			$join_table = $key.'_'.$this->table_name;
 			$f_key = inflector::singular($this->table_name).'_id';
-			$this_key = inflector::singular($key).'_id';
+			$this_key = inflector::singular($orig_key).'_id';
 
-			return $this->db->select($key.'.*')->from($key)->where($where + array($join_table.'.'.$f_key => $this->data['id']))->join($join_table, $join_table.'.'.$this_key, $key.'.id')->get()->result(TRUE, inflector::singular(ucwords($key)).'_Model');
+			return $this->db->select($key.'.*')->from($key)->orderby($order_by, $order)->where($where + array($join_table.'.'.$f_key => $this->data['id']))->join($join_table, $join_table.'.'.$this_key, $key.'.id')->get()->result(TRUE, inflector::singular(ucwords($key)).'_Model');
 		}
 	}
 
 	// Value is an ID
 	public function has($key, $value)
 	{
+		$original_key = $key;
 		if (isset($this->aliases[$key]))
 			$key = $this->aliases[$key];
 		$table_name = isset($this->aliases[$this->table_name]) ? $this->aliases[$this->table_name] : $this->table_name;
 
 		$join_table = $table_name.'_'.$key;
-		$f_key = inflector::singular($key).'_id';
+		$f_key = inflector::singular($original_key).'_id';
 		$this_key = inflector::singular($table_name).'_id';
 
-		if (in_array($key, $this->has_many))
+		if (in_array($original_key, $this->has_many))
 		{
 			return (bool) $this->db->select($key.'.id')->from($key)->where(array($join_table.'.'.$this_key => $this->data['id'], $join_table.'.'.$f_key => $value))->join($join_table, $join_table.'.'.$f_key, $key.'.id')->get()->count();
 		}
@@ -135,20 +140,21 @@ class Auto_Modeler_ORM_Core extends Auto_Modeler
 		if (isset($this->aliases[$key]))
 			$key = $this->aliases[$key];
 
-		return $this->db->delete($this->table_name.'_'.inflector::plural($key), array($key.'_id' => $id));
+		return db::delete($this->table_name.'_'.inflector::plural($key), array($key.'_id' => $id, inflector::singular($this->table_name).'_id' => $this->data['id']));
 	}
 
 	// Removes all relationships of $key in the join table
 	public function remove_all($key)
 	{
+		$original_key = $key;
 		if (isset($this->aliases[$key]))
 			$key = $this->aliases[$key];
 
-		if (in_array($key, $this->has_many))
+		if (in_array($original_key, $this->has_many))
 		{
 			return $this->db->delete($this->table_name.'_'.$key, array(inflector::singular($this->table_name).'_id' => $this->id));
 		}
-		else if (in_array($key, $this->belongs_to))
+		else if (in_array($original_key, $this->belongs_to))
 		{
 			return $this->db->delete($key.'_'.$this->table_name, array(inflector::singular($this->table_name).'_id' => $this->id));
 		}
@@ -169,20 +175,19 @@ class Auto_Modeler_ORM_Core extends Auto_Modeler
 		{
 			$this->db->delete($this->table_name, array('id' => $this->data['id']));
 
-			$table_name = isset($this->aliases[$this->table_name]) ? $this->aliases[$this->table_name] : $this->table_name;
 			foreach ($this->has_many as $table)
 			{
 				$model = inflector::singular($table).'_Model';
 				$temp = new $model();
-				if ($temp->has_attribute(inflector::singular($table_name).'_id')) // one to many relationship
+				if ($temp->has_attribute(inflector::singular($this->table_name).'_id')) // one to many relationship
 				{
-					$this->db->from($table)->where(inflector::singular($table_name).'_id', $this->data['id'])->delete();
+					$this->db->from($table)->where(inflector::singular($this->table_name).'_id', $this->data['id'])->delete();
 				}
 				else // many to many relationship
 				{
 					// Now delete everything from the join tables
-					$join_table = $table_name.'_'.$table;
-					$this_key = inflector::singular($table_name).'_id';
+					$join_table = $this->table_name.'_'.$table;
+					$this_key = inflector::singular($this->table_name).'_id';
 					$this->db->delete($join_table, array($this_key => $this->data['id']));
 				}
 			}
