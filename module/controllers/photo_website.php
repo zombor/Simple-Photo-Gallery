f84a4718b7b9db2b9792c6aa01b4617f5af2fe21
Lorenzo Pisani <zeelot3k@gmail.com>
2010-02-24 14:36:08 +0800

    swapping out the location of the template instead of overwriting the object on ajax calls

--- module/controllers/photo_website.php
+++ module/controllers/photo_website.php
@@ -4,11 +4,11 @@ class Photo_Website_Controller extends Website_Controller
 {
 	public function __construct()
 	{
-		parent::__construct();
-
 		if (request::is_ajax())
 		{
-			$this->template = new View('blank');
+			$this->template = 'blank';
 		}
+
+		parent::__construct();
 	}
 }
