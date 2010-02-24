8d34552d7cf30e48df602f43a852f291f1ad5166
Lorenzo Pisani <zeelot3k@gmail.com>
2010-02-24 15:54:50 +0800

    refactoring album controller

--- module/controllers/album.php
+++ module/controllers/album.php
@@ -6,18 +6,23 @@ class Album_Controller extends Photo_Website_Controller
 	{
 		javascript::add(array('jquery.colorbox.js', 'photo_effects.js'));
 		stylesheet::add(array('colorbox.css', 'colorbox-custom.css', 'photo_gallery.css'));
+
 		$this->template->title = $this->template->heading = 'Photo Gallery';
 
-		$this->template->content = new View('album/index');
-		$this->template->content->albums = Auto_Modeler_ORM::factory('album')->fetch_all('album_order');
+		$this->template->content = View::factory('album/index')
+			->bind('albums', $albums)
+			->bind('user', $user);
+
+		$albums = Auto_Modeler_ORM::factory('album')->fetch_all('album_order');
+
+		// Either an object or FALSE
+		$user = Auth::instance()->get_user();
 	}
 
 	public function view($album_name = NULL)
 	{
 		javascript::add(array('jquery.colorbox.js', 'photo_effects.js'));
 		stylesheet::add(array('colorbox.css', 'colorbox-custom.css', 'photo_gallery.css'));
-
-		$page_num = Input::instance()->get('page', 1);
 
 		$album = new Album_Model($album_name);
 		if ( ! $album->id)
@@ -26,9 +31,18 @@ class Album_Controller extends Photo_Website_Controller
 		$this->template->title = $album->album_name;
 		$this->template->heading = $album->album_name;
 
-		$this->template->content = new View('album/view');
-		$this->template->content->photos = $album->find_photos($page_num);
-		$this->template->content->album = $album;
-		$this->template->content->num_pages = ceil(count($album->find_related('photos')) / Kohana::config('photo_gallery.photos_per_page'));
+		$page_num = Input::instance()->get('page', 1);
+
+		$this->template->content = View::factory('album/view')
+			->bind('photos', $photos)
+			->bind('album', $album)
+			->bind('num_pages', $num_pages)
+			->bind('user', $user);
+
+		$photos = $album->find_photos($page_num);
+		$num_pages = ceil(count($album->find_related('photos')) / Kohana::config('photo_gallery.photos_per_page'));
+
+		// Either an object or FALSE
+		$user = Auth::instance()->get_user();
 	}
 }
